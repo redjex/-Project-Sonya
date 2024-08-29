7ff98840726e4bb56549b26e5df0941926f18517
redjex <redjexs@gmail.com>
2024-08-29 16:58:12 +0300

    Update SonyaControl.py

--- DesctopExe/SonyaControl.py
+++ DesctopExe/SonyaControl.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 import serial
 import time
 from tkinter import messagebox
+import random
+from tkinter import PhotoImage
 
 
 class CustomApp(ctk.CTk):
@@ -10,6 +12,7 @@ class CustomApp(ctk.CTk):
         ctk.set_appearance_mode("dark")
         self.title("LED Control App")
         self.geometry("1000x550")
+        self.iconbitmap("C:\\Users\\micha\\OneDrive\\Рабочий стол\\Sonya\\home.ico")
         self.configure(bg='#f0f0f0')
 
         # Подключение к Arduino
@@ -20,36 +23,57 @@ class CustomApp(ctk.CTk):
             messagebox.showerror("Connection Error", f"Could not connect to Arduino: {e}")
             self.destroy()  # Закрываем приложение, если не удается подключиться
         # Создаем кнопку для включения светодиода
+
         self.button_on = ctk.CTkButton(
             self,
-            text="Left eye",
+            text="Левый глаз",
             command=self.turn_led_on,
             font=("Arial", 14),
             fg_color='#28a745',  # Зеленый цвет для включения
             hover_color='#218838'
         )
-        self.button_on.pack(pady=20, anchor="nw")
+        self.button_on.grid(pady=0, padx=0, row=0, column=3)
 
         # Создаем кнопку для выключения светодиода
         self.button_off = ctk.CTkButton(
             self,
-            text="Right eye",
+            text="Правый глаз",
             command=self.turn_led_off,
             font=("Arial", 14),
             fg_color='#dc3545',  # Красный цвет для выключения
             hover_color='#c82333'
         )
-        self.button_off.pack(pady=20, anchor="nw")
+        self.button_off.grid(pady=0, padx=0, row=0, column=2)
 
         self.button_off = ctk.CTkButton(
             self,
-            text="Blink",
+            text="Радость",
             command=self.blink_led,
             font=("Arial", 14),
             fg_color='#000000',  # Красный цвет для выключения
             hover_color='#171717'
         )
-        self.button_off.pack(pady=20, anchor="nw")
+        self.button_off.grid(pady=0, padx=0, row=0, column=1)
+
+        self.button_off = ctk.CTkButton(
+            self,
+            text="Грусть",
+            command=self.sadness,
+            font=("Arial", 14),
+            fg_color='#000000',  # Красный цвет для выключения
+            hover_color='#171717'
+        )
+        self.button_off.grid(pady=0, padx=0, row=0, column=4)
+
+        self.button_off = ctk.CTkButton(
+            self,
+            text="Рамдомные эмоции",
+            command=self.random,
+            font=("Arial", 14),
+            fg_color='#000000',  # Красный цвет для выключения
+            hover_color='#171717'
+        )
+        self.button_off.grid(pady=0, padx=0, row=0, column=5)
 
         self.button_off = ctk.CTkButton(
             self,
@@ -59,7 +83,7 @@ class CustomApp(ctk.CTk):
             fg_color='#000000',  # Красный цвет для выключения
             hover_color='#171717'
         )
-        self.button_off.pack(pady=150, anchor="se")
+        self.button_off.grid(pady=490, padx=430, row=4, column=10)
 
     def turn_led_on(self):
         self.send_command('1')
@@ -70,9 +94,18 @@ class CustomApp(ctk.CTk):
     def blink_led(self):
         self.send_command('2')
 
+    def sadness(self):
+        self.send_command('3')
+
+    def random(self):
+        random_number = random.randint(0, 3)
+        # Отправка сгенерированного числа в функцию send_command
+        for i in range(3):
+            self.send_command(random_number)
+
     def send_command(self, command):
         try:
-            self.arduino.write(command.encode())
+            self.arduino.write(f"{command}\n".encode())
             time.sleep(0.1)  # Небольшая задержка для обработки команды
         except Exception as e:
             messagebox.showerror("Communication Error", f"Error sending command to Arduino: {e}")
@@ -83,7 +116,6 @@ class CustomApp(ctk.CTk):
         except Exception as e:
             print(f"Error closing serial connection: {e}")
         self.destroy()
-
 
 if __name__ == "__main__":
     app = CustomApp()
